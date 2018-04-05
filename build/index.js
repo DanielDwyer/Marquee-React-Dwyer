8d3712f31922bb3b535242a3fee69483a74bf5c3
DanielDwyer <danielpatrickdwyer@gmail.com>
2018-04-05 08:22:45 -0600

    compiled prod

--- build/index.js
+++ build/index.js
@@ -100,13 +100,6 @@ module.exports = require("react");
 />
 */
 
-/*
-@keyframes marquee {
-    0%   { transform: translate(0, 0); animation-timing-function: ease-in;}
-    100% { transform: translate(-100%, 0); animation-timing-function: ease-out;}
-}
-*/
-
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
@@ -125,8 +118,6 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var counter = 0;
-
 var Marquee = function (_React$Component) {
   _inherits(Marquee, _React$Component);
 
@@ -136,137 +127,108 @@ var Marquee = function (_React$Component) {
     var _this = _possibleConstructorReturn(this, (Marquee.__proto__ || Object.getPrototypeOf(Marquee)).call(this, props));
 
     _this.state = {
-      marquee: _this.props.Index0,
-      marqueeArr: []
+      totalDisplays: 10,
+      display1: 'a',
+      display2: 'b',
+      display3: 'c',
+      display4: 'd',
+      display5: 'e',
+      display6: 'f',
+      display7: 'g',
+      display8: 'h',
+      display9: 'i',
+      display10: 'j',
+      changeTime: 1000,
+      crossTime: 10000,
+      randomDisplayChange: false,
+      htmlTag: 'h1',
+      color: 'purple',
+      currentlyDisplayedIndex: 1,
+      currentlyDisplayedData: 'loading...'
     };
-    _this.changeMarqueeI = _this.changeMarqueeI.bind(_this);
-    _this.changeMarqueeII = _this.changeMarqueeII.bind(_this);
+    _this.marqueeDisplayChange = _this.marqueeDisplayChange.bind(_this);
     return _this;
   }
 
   _createClass(Marquee, [{
-    key: 'componentWillMount',
-    value: function componentWillMount() {
-
-      /*
-      +++ Uncomment any line(s) to see the props being passed to the marquee
-      */
-
-      // console.log("this.props\n",this.props);//all props
-      // console.log("this.props.size:",this.props.Size);//html element p, h1, h6 ...
-      // console.log("this.props.NumberOfOptions:",this.props.NumberOfOptions);//number of indices in the array of marquee sayings
-      // console.log("this.props.Index0:",this.props.Index0);
-      // console.log("this.props.Index1:",this.props.Index1);
-      // console.log("this.props.Index2:",this.props.Index2);
-      // console.log("this.props.Index3:",this.props.Index3);
-      // console.log("this.props.Index4:",this.props.Index4);
-      // console.log("this.props.index5:",this.props.Index5);
-      // console.log("this.props.Index6:",this.props.Index6);
-      // console.log("this.props.Index7:",this.props.Index7);
-      // console.log("this.props.Index8:",this.props.Index8);
-      // console.log("this.props.Index9:",this.props.Index9);
-      // console.log("this.props.Index10:",this.props.Index10);
-      // console.log("this.props.TimeToCross:",this.props.TimeToCross);
-      // console.log("this.props.TimeToChange:",this.props.TimeToChange);
-      // console.log("this.props.IsRandom",this.props.IsRandom);
-
-      var numberOfOptions = this.props.NumberOfOptions;
-      var isRandom = this.props.IsRandom;
-
-      //for loop to place marquee sayings into the placeholder array for easy access either in order or randomly
-      for (var i = 0; i < Number(numberOfOptions); i++) {
-        // console.log("looping:",i);
-        if (i === 0) {
-          this.state.marqueeArr.push(this.props.Index0);
-          // console.log("pushed Index 0=>",this.props.Index0);
-        } else if (i === 1) {
-          this.state.marqueeArr.push(this.props.Index1);
-          // console.log("pushed Index 1=>",this.props.Index1);
-        } else if (i === 2) {
-          this.state.marqueeArr.push(this.props.Index2);
-          // console.log("pushed Index 2=>",this.props.Index2);
-        } else if (i === 3) {
-          this.state.marqueeArr.push(this.props.Index3);
-          // console.log("pushed Index 3=>",this.props.Index3);
-        } else if (i === 4) {
-          this.state.marqueeArr.push(this.props.Index4);
-          // console.log("pushed Index 4=>",this.props.Index4);
-        } else if (i === 5) {
-          this.state.marqueeArr.push(this.props.Index5);
-          // console.log("pushed Index 5=>",this.props.Index5);
-        } else if (i === 6) {
-          this.state.marqueeArr.push(this.props.Index6);
-          // console.log("pushed Index 6=>",this.props.Index6);
-        } else if (i === 7) {
-          this.state.marqueeArr.push(this.props.Index7);
-          // console.log("pushed Index 7=>",this.props.Index7);
-        } else if (i === 8) {
-          this.state.marqueeArr.push(this.props.Index8);
-          // console.log("pushed Index 8=>",this.props.Index8);
-        } else if (i === 9) {
-          this.state.marqueeArr.push(this.props.Index9);
-          // console.log("pushed Index 9=>",this.props.Index9);
-        } else if (i === 10) {
-          this.state.marqueeArr.push(this.props.Index10);
-          // console.log("pushed Index 10=>",this.props.Index10);
-        }
-      }
-
-      //some things need to be numbers not strings
-      var timeToChange = Number(this.props.TimeToChange); //the time in ms that each marquee saying changes
-
-      if (this.props.IsRandom == "true") {
-        var changeMarquee = setInterval(this.changeMarqueeI, timeToChange);
+    key: 'componentDidMount',
+    value: function componentDidMount() {
+      if (this.props.IsRandom === "false") {
+        var rand = false;
       } else {
-        var changeMarquee = setInterval(this.changeMarqueeII, timeToChange);
+        var rand = true;
       }
+      this.setState({
+        currentlyDisplayed: this.props.Index0,
+        totalDisplays: this.props.NumberOfOptions || 1,
+        changeTime: this.props.TimeToChange || 2000,
+        crossTime: this.props.TimeToCross || 8000,
+        randomDisplayChange: rand || false,
+        htmlTag: this.props.Size || 'h3',
+        color: this.props.Color || 'purple'
+      });
+      this.props.Index0 ? this.setState({ display1: this.props.Index0 }) : delete this.state.display1;
+      this.props.Index1 ? this.setState({ display2: this.props.Index1 }) : delete this.state.display2;
+      this.props.Index2 ? this.setState({ display3: this.props.Index2 }) : delete this.state.display3;
+      this.props.Index3 ? this.setState({ display4: this.props.Index3 }) : delete this.state.display4;
+      this.props.Index4 ? this.setState({ display5: this.props.Index4 }) : delete this.state.display5;
+      this.props.Index5 ? this.setState({ display6: this.props.Index5 }) : delete this.state.display6;
+      this.props.Index6 ? this.setState({ display7: this.props.Index6 }) : delete this.state.display7;
+      this.props.Index7 ? this.setState({ display8: this.props.Index7 }) : delete this.state.display8;
+      this.props.Index8 ? this.setState({ display9: this.props.Index8 }) : delete this.state.display9;
+      this.props.Index9 ? this.setState({ display10: this.props.Index9 }) : delete this.state.display10;
+
+      setTimeout(function () {
+        this.marqueeDisplayChange();
+      }.bind(this), 250);
     }
   }, {
-    key: 'changeMarqueeI',
-    value: function changeMarqueeI() {
-      var randomIndex = (Math.random() * (this.props.NumberOfOptions - 1)).toFixed(0);
-      // console.log(this.state.marqueeArr[randomIndex]);
-      this.setState({ marquee: this.state.marqueeArr[randomIndex] });
-    }
-  }, {
-    key: 'changeMarqueeII',
-    value: function changeMarqueeII() {
-      // console.log(this.state.marqueeArr[counter]);
-      this.setState({ marquee: this.state.marqueeArr[counter] });
-      counter++;
-      if (counter > this.props.NumberOfOptions - 1) {
-        counter = 0;
+    key: 'marqueeDisplayChange',
+    value: function marqueeDisplayChange() {
+      if (this.state.randomDisplayChange) {
+
+        var that = this;
+        var intervalTime = this.state.changeTime;
+
+        setInterval(function () {
+          var randomIndex = Math.floor(Math.random() * that.state.totalDisplays + 1);
+          that.setState({
+            currentlyDisplayedData: that.state["display" + randomIndex]
+          });
+        }, intervalTime);
+      } else {
+
+        var that = this;
+        var intervalTime = this.state.changeTime;
+
+        setInterval(function () {
+          var nextIndex = that.state.currentlyDisplayedIndex;
+          var updateStateWith = nextIndex + 1;
+          if (updateStateWith == Number(that.state.totalDisplays) + 1) {
+            updateStateWith = 1;
+          } else {
+            updateStateWith = nextIndex + 1;
+          }
+          that.setState({ currentlyDisplayedData: that.state["display" + nextIndex], currentlyDisplayedIndex: updateStateWith });
+        }, intervalTime);
       }
     }
   }, {
     key: 'render',
     value: function render() {
-
-      var marquee = this.state.marquee;
-      // console.log("render:",marquee);
-
-      var styling1 = {
-        margin: '0 auto',
-        whiteSpace: 'nowrap',
-        overflow: 'hidden'
-      };
-
-      var styling2 = {
-        display: 'inline-block',
-        paddingLeft: '100%',
-        animation: 'marquee ' + Number(this.props.TimeToCross) + 'ms linear infinite',
-        color: this.props.Color
-      };
+      var marquee = this.state.currentlyDisplayedData;
+      var style1 = { margin: '0 auto', whiteSpace: 'nowrap', overflow: 'hidden' };
+      var style2 = { display: 'inline-block', paddingLeft: '100%', animation: 'marquee ' + Number(this.state.crossTime) + 'ms linear infinite', color: this.state.color };
 
       return _react2.default.createElement(
         'section',
         { id: 'marquee' },
         _react2.default.createElement(
-          this.props.Size,
-          { style: styling1 },
+          this.state.htmlTag,
+          { style: style1 },
           _react2.default.createElement(
             'span',
-            { style: styling2 },
+            { style: style2 },
             marquee
           )
         )
